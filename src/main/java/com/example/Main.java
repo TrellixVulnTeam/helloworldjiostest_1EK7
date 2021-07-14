c6d2af3730ceb40cdf575fd33d7cd4fb12302147
Eva02Daruma <70488300+Eva02Daruma@users.noreply.github.com>
2021-07-14 09:43:38 -0400

    make it better

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -25,10 +25,7 @@ import org.jscience.physics.model.RelativisticModel;
 import org.jscience.physics.amount.Amount;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -47,7 +44,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -141,46 +137,5 @@ public class Main {
 
 }
 
-//public void class AccessingDataMongodbApplication implements CommandLineRunner {
-//
-//  @Autowired
-//  private ProductosRepository repository;
-//
-//  public static void main(String[] args) {
-//    SpringApplication.run(AccessingDataMongodbApplication.class, args);
-//  }
-//
-//  @Override
-//  public void run(String... args) throws Exception {
-//
-//    repository.deleteAll();
-//
-//    // save a couple of customers
-//     //Estructura :
-//    //01,20,"Manzana","A23",850
-//    repository.save(new Producto(01,20,"Manzana","A23",850.0));
-//    repository.save(new Producto(02,15,"Pera","B24",650.0));
-//
-//    // fetch all customers
-//    System.out.println("Producto encontrado por findAll():");
-//    System.out.println("-------------------------------");
-//    for (Producto producto : repository.findAll()) {
-//      System.out.println(producto);
-//    }
-//    System.out.println();
-//
-//    // fetch an individual customer
-//    System.out.println("Customer found with findByFirstName('Alice'):");
-//    System.out.println("--------------------------------");
-//    System.out.println(repository.findByName("Manzana"));
-//
-//    System.out.println("Customers found with findByLastName('Smith'):");
-//    System.out.println("--------------------------------");
-//    for (Producto producto : repository.findBycode("B24")) {
-//      System.out.println(producto);
-//    }
-//
-//  }
-//
-//}
+
 
