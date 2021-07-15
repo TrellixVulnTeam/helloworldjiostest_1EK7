c7ed68192ee3ffe3feb3c3f1ad902498552d81f9
Eva02Daruma <70488300+Eva02Daruma@users.noreply.github.com>
2021-07-15 10:02:42 -0400

    make it better

--- src/main/java/com/example/Producto.java
+++ src/main/java/com/example/Producto.java
@@ -18,20 +18,22 @@ public class Producto {
     private Integer id, stock;
     private String nombre, codigo;
     private Double precio;
+    private categoriaProducto categoriaproducto;
 
     
     //Estructura :
     //01,20,"Manzana","A23",850
    
     public Producto(){
-
+      
     }
-    public Producto(Integer idd,Integer stockc, String nom, String cod, Double prec){
+    public Producto(Integer idd,Integer stockc, String nom, String cod, Double prec,categoriaProducto cgprod){
         this.id = idd;
         this.stock = stockc;
         this.nombre = nom;
         this.codigo = cod;
         this.precio= prec;
+        this.categoriaproducto=cgprod;
     }
 
     public Integer getId() {
@@ -73,12 +75,20 @@ public class Producto {
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
+
+    public categoriaProducto getCategoriaproducto() {
+        return categoriaproducto;
+    }
+
+    public void setCategoriaproducto(categoriaProducto categoriaproducto) {
+        this.categoriaproducto = categoriaproducto;
+    }
     
     
     public String toString(){
         return String.format(
-        "Producto[id=%s, stock =%s , nombre ='%s',codigo = '%s',precio =%s]",
-                id,stock,nombre,codigo,precio);
+        "Producto[id=%s, stock =%s , nombre ='%s',codigo = '%s',precio =%s ,categoriaproducto = '%s' ]",
+                id,stock,nombre,codigo,precio, categoriaproducto);
         
     }
 }
